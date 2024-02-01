69020dfbe25f9c9e8cf1b7345ee2b850e7ed75cd
WasinUddy <wasinsilakong@gmail.com>
2024-02-01 13:17:48 +0700

    Fiber serving static files

--- main.go
+++ main.go
@@ -17,13 +17,17 @@ func main() {
 		return c.SendString("Hello, World 👋!")
 	})
 
+	app.Get("/login", func(c *fiber.Ctx) error { return c.SendFile("./views/login.html") })
 	app.Post("/login", handlers.Login)
+
 	app.Post("/register", handlers.Register)
+	app.Get("/register", func(c *fiber.Ctx) error { return c.SendFile("./views/register.html") })
 
 	app.Get("/:s", handlers.Redirect)
 
 	app.Use(middlewares.Protected())
 	app.Post("/shorten", handlers.Shorten)
+	app.Get("/shorten", func(c *fiber.Ctx) error { return c.SendFile("./views/shorten.html") })
 
 	app.Listen("0.0.0.0:3000")
 }
